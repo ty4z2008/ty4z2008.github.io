655e298b6ecbf777c2a5fd397dbc4d7dcf866ac5
Jeffery <jeffery@influx.io>
2018-12-02 17:01:20 +0800

    merge

--- js/hux-blog.js
+++ js/hux-blog.js
@@ -18,11 +18,7 @@
 
 
 // make all images responsive
-/* 
- * Unuse by Hux
- * actually only Portfolio-Pages can't use it and only post-img need it.
- * so I modify the _layout/post and CSS to make post-img responsive!
- */
+
 // $(function() {
 //  $("img").addClass("img-responsive");
 // });
